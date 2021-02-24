f23aa489c245d80cbb32586179fa4fe5364a686c
Candice James-Waples <76015275+CTJW@users.noreply.github.com>
2021-02-23 22:04:54 -0500

    working getting items on the page, before official  directions

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -2,11 +2,25 @@ import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Productcard from "./Productcard";
 
 function App() {
   return (
-    
-  )
+    <div>
+      <Productcard></Productcard>;
+    </div>
+  );
 }
-
-export default App;
+// export default function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Switch>
+//           <Route exact path="/" component={Contact}></Route>
+//           <Route path="/detail/:id" component={ContactDetail}></Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
---
+++ shopping-cart/src/Productcard.js
@@ -0,0 +1,38 @@
+import dbJSON from "./db.json";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import React from "react";
+import "./Apps.css";
+
+export default function Productcard(props) {
+  return (
+    <div className="productcard">
+      {/* <p>{props.phone2}</p> */}
+      <p>{props.user.name.first + " " + props.user.name.last}</p>
+      <p>{props.user.phone == null ? "N/A" : props.user.phone}</p>
+    </div>
+  );
+}
+
+// const theProducts = items.results;
+
+// // console.log(theProducts)
+
+// const theProductsHTML = theProducts.map(function (item) {
+//   return `
+//     <div class = "product">
+//     <img src="${item.Images[0].url_fullxfull}" class ="product-bg">
+//  <p class="title">${item.title}</p>
+//  <p class="shop">${item.Shop.shop_name}</p>
+// <span class="views"> views (${item.views})</span>
+// <p class="price"><strong>$${item.price}</strong></p>
+//      </div>`;
+// });
+
+// document.querySelector("#grid").innerHTML = theProductsHTML.join("");
+
+// products.img.normal
+// products.title
+//products.price
+//products.installments
+//A button
